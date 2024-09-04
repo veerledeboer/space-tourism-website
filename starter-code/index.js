b4bfc8236d90eec48f3416e2c4c5cd81f054d1cf
veerledeboer <deboerveerle@gmail.com>
2024-09-04 15:56:51 +0200

    finished home page

---
+++ starter-code/index.js
@@ -0,0 +1,19 @@
+// when someone clicks the hamburger button
+// if the nav is closed, open it
+// if the nav is open, close it
+// also class change from icons
+
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = nav.getAttribute("data-visible");
+    if (visibility === "false"){
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+})
+
